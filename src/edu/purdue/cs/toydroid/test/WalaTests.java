18d0034f40be39151b62b31da0c803e6331d0c59
leonard <anon@anon.x>
2024-05-27 22:00:14 +0200

    Wala Tests
    Former-commit-id: db016bf60bc7d4ca2f08ab7129107ab2dc20308b
    Former-commit-id: d6368c086441c5d827c31551d1de165581c95a2c

---
+++ src/edu/purdue/cs/toydroid/test/WalaTests.java
@@ -0,0 +1,13 @@
+package edu.purdue.cs.toydroid.test;
+
+import com.ibm.wala.examples.drivers.PDFWalaIR;
+
+import java.io.IOException;
+
+public class WalaTests {
+
+    public static void main(String[] args) throws IOException {
+//        System.out.println("Path separator: " + File.pathSeparator);
+        PDFWalaIR.main(args);
+    }
+}
