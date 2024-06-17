629f6988d3b8bb3e2aafad3d48f5a3e203b864f6
leonard <anon@anon.x>
2024-06-17 16:16:27 +0200

    Extract global string constant processing
    Former-commit-id: eb0943f1127fcb47e772fe3e694dd244287eeb7c
    Former-commit-id: 69028dda2eab29c7a4fe642bca5b287d2ab17b2b

--- src/edu/purdue/cs/toydroid/bidtext/graph/TypingGraphUtil.java
+++ src/edu/purdue/cs/toydroid/bidtext/graph/TypingGraphUtil.java
@@ -12,7 +12,6 @@ import com.ibm.wala.ipa.slicer.HeapStatement.HeapReturnCaller;
 import com.ibm.wala.ipa.slicer.*;
 import com.ibm.wala.ipa.slicer.Statement.Kind;
 import com.ibm.wala.ssa.*;
-import com.ibm.wala.types.TypeReference;
 import com.ibm.wala.util.graph.Graph;
 import edu.purdue.cs.toydroid.bidtext.analysis.AnalysisUtil;
 import edu.purdue.cs.toydroid.bidtext.analysis.InterestingNode;
@@ -87,7 +86,7 @@ public class TypingGraphUtil {
         logger.info("   - Process possible incoming fields");
         graph.collectIncomingFields();
         logger.info("   - Revisit TypingGraph for global constants");
-        revisitTypingGraph(graph);
+        new GlobalConstantStringProcessor(graph).revisitTypingGraph();
         // for debug purpose
         // graph.dumpEntrypointTypingGraph();
         logger.info("   - Update Typing Records for Fields");
@@ -97,16 +96,6 @@ public class TypingGraphUtil {
         // clear typing graph at the end - remove usused data for memory
         // efficiency
         graph.clearAtEnd();
-    }
-
-    // ///////////////////////////
-    private static void revisitTypingGraph(TypingGraph graph) {
-        Iterator<CGNode> allCGNodes = graph.iterateAllCGNodes();
-        while (allCGNodes.hasNext()) {
-            CGNode cgNode = allCGNodes.next();
-            TypingSubGraph sg = graph.findOrCreateSubGraph(cgNode);
-            revisitPotentialGString(cgNode, sg);
-        }
     }
 
     private static void buildTypingGraphForStmt(CallGraph cg, Graph<Statement> sdg, Statement stmt,
@@ -822,231 +811,6 @@ public class TypingGraphUtil {
             useRec.addForwardTypingConstraint(c);
             c.addPath(stmt);
             // currentTypingGraph.mergeClass(useNode, defNode);
-        }
-    }
-
-    /************************************************************/
-    /****************** Handle Constant String ******************/
-    /************************************************************/
-    private static void revisitPotentialGString(CGNode cgNode, TypingSubGraph sg) {
-        Set<SSAAbstractInvokeInstruction> gStringLoc = sg.potentialGStringLocations();
-        if (gStringLoc == null) {
-            return;
-        }
-        IR ir = cgNode.getIR();
-        SymbolTable symTable = ir.getSymbolTable();
-        SSACFG cfg = ir.getControlFlowGraph();
-        SSACFG.BasicBlock bb = cfg.getBasicBlock(2);
-        // System.out.println(ir);
-        for (SSAAbstractInvokeInstruction inst : gStringLoc) {
-            revisitInvokeInstruction(cfg, symTable, inst, sg);
-        }
-    }
-
-    private static void revisitInvokeInstruction(SSACFG cfg, SymbolTable symTable, SSAAbstractInvokeInstruction inst,
-                                                 TypingSubGraph sg) {
-        List<TypingNode> potentialGNodes = potentialGStringNode(symTable, inst, sg);
-        if (potentialGNodes != null) {
-            SSACFG.BasicBlock locatedBB = cfg.getBlockForInstruction(inst.iIndex());
-            List<ISSABasicBlock> worklist = new LinkedList<>();
-            Set<ISSABasicBlock> storedBBs = new HashSet<>();
-            worklist.add(locatedBB);
-            forwardObtainBBsInLine(cfg, worklist, storedBBs);
-            for (ISSABasicBlock bb : storedBBs) {
-                typingGlobalStringInBB(cfg, symTable, bb, potentialGNodes, sg, inst, true);
-            }
-            worklist.add(locatedBB);
-            storedBBs.clear();
-            backwardObtainBBsInLine(cfg, worklist, storedBBs);
-            storedBBs.remove(locatedBB);
-            for (ISSABasicBlock bb : storedBBs) {
-                typingGlobalStringInBB(cfg, symTable, bb, potentialGNodes, sg, inst, false);
-            }
-        }
-    }
-
-    private static void forwardObtainBBsInLine(SSACFG cfg, List<ISSABasicBlock> worklist,
-                                               Set<ISSABasicBlock> storedBBs) {
-        ISSABasicBlock bb;
-        if (worklist.isEmpty()) {
-            return;
-        }
-        bb = worklist.removeFirst();
-        if (bb.isExitBlock() || cfg.getPredNodeCount(bb) > 1) {
-            return;
-        }
-        storedBBs.add(bb);
-        SSAInstruction lastInst = bb.getLastInstruction();
-        if (!(lastInst instanceof SSAConditionalBranchInstruction) && !(lastInst instanceof SSASwitchInstruction)) {
-            Iterator<ISSABasicBlock> succNodes = cfg.getSuccNodes(bb);
-            while (succNodes.hasNext()) {
-                ISSABasicBlock succ = succNodes.next();
-                if (succ.isCatchBlock() || succ.isExitBlock()) {
-                    continue;
-                }
-                worklist.add(succ);
-                break;
-            }
-        }
-        forwardObtainBBsInLine(cfg, worklist, storedBBs);
-    }
-
-    private static void backwardObtainBBsInLine(SSACFG cfg, List<ISSABasicBlock> worklist,
-                                                Set<ISSABasicBlock> storedBBs) {
-        ISSABasicBlock bb;
-        if (worklist.isEmpty()) {
-            return;
-        }
-        bb = worklist.removeFirst();
-        if (bb.isEntryBlock() || cfg.getPredNodeCount(bb) > 1) {
-            return;
-        }
-        storedBBs.add(bb);
-        Iterator<ISSABasicBlock> predNodes = cfg.getPredNodes(bb);
-        while (predNodes.hasNext()) {
-            ISSABasicBlock pred = predNodes.next();
-            if (pred.isEntryBlock()) {
-                continue;
-            }
-            SSAInstruction lastInst = pred.getLastInstruction();
-            if ((lastInst instanceof SSAConditionalBranchInstruction) || (lastInst instanceof SSASwitchInstruction)) {
-                storedBBs.add(pred);
-            } else {
-                worklist.add(pred);
-            }
-        }
-        backwardObtainBBsInLine(cfg, worklist, storedBBs);
-    }
-
-    private static List<TypingNode> potentialGStringNode(SymbolTable symTable, SSAAbstractInvokeInstruction inst,
-                                                         TypingSubGraph sg) {
-        List<TypingNode> nodes = null;
-        String sig = WalaUtil.getSignature(inst);
-        String loc = TypingGraphConfig.getPotentialGStringLoc(sig);
-        if (loc != null) {
-            String[] indices = loc.split(TypingGraphConfig.SEPERATOR);
-            for (String idx : indices) {
-                int lc = Integer.parseInt(idx);
-                int useVal = inst.getUse(lc);
-                if (symTable.isConstant(useVal)) {
-                    TypingNode node = null;
-                    if (!inst.isStatic()) {
-                        lc -= 1;
-                    }
-                    TypeReference type = inst.getDeclaredTarget().getParameterType(lc);
-                    if (type.isPrimitiveType()) {
-                        // TODO
-                    } else if (symTable.isStringConstant(useVal)) {
-                        node = sg.findOrCreate(useVal);
-                    }
-                    if (node != null) {
-                        if (nodes == null) {
-                            nodes = new LinkedList<>();
-                        }
-                        nodes.add(node);
-                    }
-                }
-            }
-        }
-
-        return nodes;
-    }
-
-    // remove support for normal definition stmt; only support branch stmt.
-    private static void typingGlobalStringInBB(SSACFG cfg, SymbolTable symTable, ISSABasicBlock bb,
-                                               List<TypingNode> potentialGNodes, TypingSubGraph sg,
-                                               SSAAbstractInvokeInstruction invoke, boolean forward) {
-        if (forward) {
-            for (int i = bb.getFirstInstructionIndex(); i <= bb.getLastInstructionIndex(); i++) {
-                SSAInstruction inst = cfg.getInstructions()[i];
-                if (inst == null || inst instanceof SSAConditionalBranchInstruction ||
-                        inst instanceof SSASwitchInstruction) {
-                    break;
-                }
-                // System.err.println("[" + i + "]" + inst);
-                // typingGlobalStringInInstruction(symTable, inst,
-                // potentialGNodes, sg);
-            }
-        } else {
-            for (int i = bb.getLastInstructionIndex(); i >= bb.getFirstInstructionIndex(); i--) {
-                SSAInstruction inst = cfg.getInstructions()[i];
-                if (inst == null) {
-                    break;
-                } else if (inst instanceof SSAConditionalBranchInstruction || inst instanceof SSASwitchInstruction) {
-                    typingGlobalStringInInstruction(symTable, inst, potentialGNodes, sg, invoke);
-                    break;
-                }
-                // typingGlobalStringInInstruction(symTable, inst,
-                // potentialGNodes, sg);
-            }
-        }
-    }
-
-    private static void typingGlobalStringInInstruction(SymbolTable symTable, SSAInstruction inst,
-                                                        List<TypingNode> potentialGNodes, TypingSubGraph sg,
-                                                        SSAAbstractInvokeInstruction invoke) {
-        int val = -1, extraVal = -1;
-        if (inst instanceof SSAPutInstruction ssaPut) {
-            val = ssaPut.getVal();
-        } else if (inst instanceof SSAArrayStoreInstruction ssaArrayStore) {
-            val = ssaArrayStore.getValue();
-        } else if (inst instanceof SSAConditionalBranchInstruction ssaConditionalBranch) {
-            val = ssaConditionalBranch.getUse(0);
-            extraVal = ssaConditionalBranch.getUse(1);
-        } else if (inst instanceof SSASwitchInstruction ssaSwitch) {
-            val = ssaSwitch.getUse(0);
-        } else if (inst.hasDef()) {
-            val = inst.getDef();
-        }
-        typingGlobalStringInInstruction_processNode(symTable, potentialGNodes, sg, invoke, val);
-        typingGlobalStringInInstruction_processNode(symTable, potentialGNodes, sg, invoke, extraVal);
-    }
-
-    private static void typingGlobalStringInInstruction_processNode(SymbolTable symTable,
-                                                                    List<TypingNode> potentialGNodes, TypingSubGraph sg,
-                                                                    SSAAbstractInvokeInstruction invoke, int val) {
-        TypingNode valNode;
-        if (val != -1 && !symTable.isConstant(val)) {
-            valNode = sg.find(val);
-            if (valNode != null) {
-                TypingRecord valRec = currentTypingGraph.findOrCreateTypingRecord(valNode.getGraphNodeId());
-                for (TypingNode n : potentialGNodes) {
-                    // currentTypingGraph.mergeClass(n, valNode);
-                    TypingRecord rec = currentTypingGraph.findOrCreateTypingRecord(n.getGraphNodeId());
-                    TypingConstraint c =
-                            new TypingConstraint(valNode.getGraphNodeId(), TypingConstraint.GE, n.getGraphNodeId());
-                    // a simply way to get the corresponding statement of the
-                    // alert inst to act as the path element (I think it usually
-                    // fails)
-                    Set<TypingConstraint> fc = rec.getForwardTypingConstraints();
-                    for (TypingConstraint tc : fc) {
-                        List<Statement> path = tc.getPath();
-                        boolean found = false;
-                        for (Statement p : path) {
-                            if (p.getKind() == Kind.PARAM_CALLER) {
-                                ParamCaller pcaller = (ParamCaller) p;
-                                if (pcaller.getInstruction().equals(invoke)) {
-                                    c.addPath(pcaller);
-                                    found = true;
-                                    break;
-                                }
-                            } else if (p.getKind() == Kind.NORMAL_RET_CALLER) {
-                                NormalReturnCaller nrc = (NormalReturnCaller) p;
-                                if (nrc.getInstruction().equals(invoke)) {
-                                    c.addPath(nrc);
-                                    found = true;
-                                    break;
-                                }
-                            }
-                        }
-                        if (found) {
-                            break;
-                        }
-                    }
-                    rec.addForwardTypingConstraint(c);
-                    // valRec.addBackwardTypingConstraint(c);
-                }
-            }
         }
     }
 
